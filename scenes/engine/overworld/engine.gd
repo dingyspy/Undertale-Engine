8b77d80c433e9a46b4815127fbada29547f95864
dingyspy <chancebreazier@gmail.com>
2025-08-23 00:05:24 -0700

    item, info
    and some toby style fixes

--- scenes/engine/overworld/engine.gd
+++ scenes/engine/overworld/engine.gd
@@ -25,7 +25,13 @@ var can_open_menu = true
 @onready var menu_selection_item = $'../ui/menu/selection/selections/item'
 @onready var menu_selection_stat = $'../ui/menu/selection/selections/stat'
 @onready var menu_selection_cell = $'../ui/menu/selection/selections/cell'
+@onready var menuitems = $'../ui/menu/menuitems'
+@onready var items_node = $'../ui/menu/types/items'
+@onready var items_node_selections = $'../ui/menu/types/items/selections'
+@onready var items_panel = $'../ui/menu/types/items/panel'
 @onready var soul = $'../ui/soul'
+
+@onready var menuitem = preload('res://scenes/engine/battle/menuitem.tscn')
 
 var menu_is_open = false
 var engine_can_open_menu = true
@@ -34,13 +40,17 @@ var buffer = 0
 var menu_no = 0
 var prev_menu_posy = 0
 var menu_posy = 0
+var menu_posx = 0
 var selections = []
+var stat_selections = []
 
 func _ready() -> void:
 	for i in menu_selections.get_children(): selections.append(i)
+	for i in items_node_selections.get_children(): stat_selections.append(i)
 	
 	ui.visible = false
 	dialogbox.visible = false
+	items_node.visible = false
 	
 	Audio.store_audio({
 		'move' : 'res://audio/engine/snd_squeak.wav',
@@ -84,23 +94,63 @@ func _process(delta: float) -> void:
 			player.mode = prev_player_mode
 	
 	if menu_is_open:
+		if accept and buffer <= 0:
+			buffer = 2
+			match menu_no:
+				0:
+					prev_menu_posy = menu_posy
+					engine_can_open_menu = false
+					match prev_menu_posy:
+						0:
+							items_node.visible = true
+							for i in Global.items.size():
+								var item = Global.items[i]
+								create_menuitem(item['full_name'], items_panel.position + Vector2(42,28 + i * 32))
+				1:
+					menu_posx = 0
+			menu_no += 1
+			Audio.play('select')
+		
+		if cancel and buffer <= 0 and menu_no != 0:
+			buffer = 2
+			Audio.play('move')
+			match menu_no:
+				1:
+					match prev_menu_posy:
+						0:
+							items_node.visible = false
+							for i in menuitems.get_children(): i.queue_free()
+					menu_posy = prev_menu_posy
+					engine_can_open_menu = true
+			menu_no -= 1
+		
 		match menu_no:
 			0:
 				menu_posy = posmod(menu_posy + menu_y, selections.size())
 				
 				# sets the soul pos to the selection pos, you can set the offset here
-				soul.position = selections[menu_posy].position + Vector2(-19,16)
+				soul.position = selections[menu_posy].position + Vector2(-19,17)
+			1:
+				match prev_menu_posy:
+					0:
+						menu_posy = posmod(menu_posy + menu_y, Global.items.size())
+						soul.position = items_panel.position + Vector2(29,45 + menu_posy * 32)
+			2:
+				match prev_menu_posy:
+					0:
+						menu_posx = posmod(menu_posx + menu_x, stat_selections.size())
+						soul.position = stat_selections[menu_posx].position + Vector2(-12,17)
+						if menu_x != 0: Audio.play('move')
 		if menu_y != 0: Audio.play('move')
-		
-		if accept:
-			match menu_no:
-				0:
-					prev_menu_posy = menu_posy
-			menu_no += 1
-			Audio.play('select')
-		
-		if cancel:
-			Audio.play('move')
-			menu_no -= 1
 	
-	if buffer > 0: buffer -= delta
+	if buffer > 0: buffer -= delta * 30
+
+# same func as the one used in the battle engine
+func create_menuitem(text, position, color=Color(1,1,1)):
+	var inst = menuitem.instantiate()
+	inst.position = position
+	inst.text = text
+	inst.rdy = true
+	inst.modulate = color
+	menuitems.add_child(inst)
+	return inst
